ea520bb74fc7cdd506bd08f4fa792cc4434d3f92
Paul Czerwenka <p.czerwenka@ostfalia.de>
2021-11-17 20:53:43 +0100

    erste Ansätze für Datentypen Art, Karte und Spieler

--- main.hs
+++ main.hs
@@ -1,7 +1,13 @@
-data Art = KARO | HERZ | PIK | KREUZ deriving (Enum, Show)
+--TODO: Enum für den Kartentypen sinnvoll? Wenn ja, dann nachschauen, ob Enum richtig erstellt wurde
+data Art = KARO | HERZ | PIK | KREUZ deriving (Enum, Show) 
 
-data Karte = Karte {
-    name :: String,
-    art :: Art,
-    wert :: Int
+data Karte = Karte {        -- neuer Datentyp namens Karte
+    name :: String,         -- Name der Karte
+    art :: Art,             -- Art der Karte
+    wert :: Int             -- Wertigkeit der Karte
 }
+
+data Spieler = Spieler {    --neuer Datentyp namens Spieler
+    id :: Int,              -- wichtig für Auslegen bei den Runden
+    name :: String          -- Name des Spielers
+}  
