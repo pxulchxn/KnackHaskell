ec75a88b64bd460d2d404fe1b2ebfe459de6da21
Paul Czerwenka <p.czerwenka@ostfalia.de>
2021-11-17 20:01:59 +0100

    alles unnötige raus

--- main.hs
+++ main.hs
@@ -6,19 +6,3 @@ data Karte = Karte {
     wert :: Int
 }
 
-data Article = Article {name1::String, price::Int} deriving (Show, Read, Eq, Ord)
-
-hose :: Article
-hose = Article "Hose" 50
-
-hose3 :: Article
-hose3 = Article "Hose" 52
-
-hose4 :: Article
-hose4 = Article "Hose" 50
-
-newtype StrWrapper = Wrap String
-
-v :: StrWrapper
-v = Wrap "Hallo Welt"
-
